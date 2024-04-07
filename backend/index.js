a6b99b225f58d43479e70a462c1586f0f1ef91cf
Somyeh Virmani <somye01dev@gmail.com>
2024-04-07 11:14:26 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,10 @@ const app = express();
 app.use(cookieParser());
 // app.use(cors())
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://pay-u.vercel.app'); // replace '*' with the exact origin
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', true); // to allow cookies
     next();
   });
 app.use(express.json());
